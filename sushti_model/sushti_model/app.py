36cc3096bbeafdd0bb064ce4da97bd188f112581
rautvaish7 <202301100003@mitaoe.ac.in>
2025-05-02 01:04:56 +0530

    Update app.py

--- sushti_model/sushti_model/app.py
+++ sushti_model/sushti_model/app.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Electricity Saving Assistant", page_icon="⚡", layout="centered")
 
 # Load model and encoders
-model = joblib.load("model.pkl")
+model = joblib.load("sushti_model/model.pkl")
 mlb = joblib.load("appliance_encoder.pkl")
 
 # Load appliance tips
